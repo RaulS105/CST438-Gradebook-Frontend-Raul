05e5a4a283e75515c4c646a9d33fb6157386af81
Raul <rauljunior97@gmail.com>
2021-10-12 00:25:49 -0700

    updated frontend

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Gradebook from './components/Gradebook';
+import Assignment from './components/Assignment';
+import NewAssignment from './components/NewAssignment';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <AppBar position="static" color="default">
+        <Toolbar>
+           <Typography variant="h6" color="inherit">
+            Gradebook
+           </Typography>
+        </Toolbar>
+      </AppBar>
+      <BrowserRouter>
+       <Switch>
+        <Route exact path='/' component={Assignment} />
+        <Route path='/newassignment' component={NewAssignment} />
+        <Route path='/gradebook' component={Gradebook} />
+       </Switch>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/constants.js
@@ -0,0 +1,1 @@
+export const SERVER_URL = 'https://cst438gradebook-backend.herokuapp.com/';
